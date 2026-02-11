87cf296abe0a9ba16046fdb232008ced5987949e
Bono Fox <git@bono-fox.de>
2026-02-11 22:08:23 +0100

    ✨ Arch-Host angelegt

---
+++ hosts/techtornado_arch.py
@@ -0,0 +1,9 @@
+from base import HostBase
+
+
+class TechtornadoArch(HostBase):
+    def __init__(self):
+        super().__init__(
+            "techtornado-arch",
+            submodules=[],
+        )

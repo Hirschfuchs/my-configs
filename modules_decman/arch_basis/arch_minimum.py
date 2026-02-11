7b00707d83a6f913f8d46eca69de3aace045dcb8
Bono Fox <git@bono-fox.de>
2026-02-11 23:18:51 +0100

    ➕ Minimale Arch-Dependencies

---
+++ modules_decman/arch_basis/arch_minimum.py
@@ -0,0 +1,20 @@
+from ..base import SubModule
+
+
+class ArchMinimum(SubModule):
+    def __init__(self):
+        super().__init__(
+            "arch-minimum",
+            native_packages=[
+                # Linux Standard-Kernel (Alternativen: linux-lts, linux-hardened)
+                "linux",
+                # Firmwareunterstützung Kernel
+                "linux-firmware",
+                # Metapaket für Minimalsystem
+                "base",
+                # Ermöglichen von Paketbau
+                "base-devel",
+                # Netzwerkverbindung ermöglichen
+                "networkmanager"
+            ]
+        )
---
+++ modules_decman/betriebssystem.py
@@ -0,0 +1,12 @@
+from base import SubModule
+from modules_decman.arch_basis.arch_minimum import ArchMinimum
+
+
+class Betriebssystem(SubModule):
+    def __init__(self):
+        super().__init__(
+            "betriebssystem",
+            submodules=[
+                ArchMinimum()
+            ]
+        )

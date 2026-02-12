9c015d974ba1bd19d3f629bf91089f20dafece1a
Bono Fox <git@bono-fox.de>
2026-02-12 11:20:43 +0100

    ➕ Bildbearbeitungstools hinzugefügt

---
+++ modules_decman/bildbearbeitung.py
@@ -0,0 +1,16 @@
+from base import SubModule
+
+
+class Bildbearbeitung(SubModule):
+    def __init__(self):
+        super().__init__(
+            "bildbearbeitung",
+            native_packages=[
+                # Lightroom-Alternative
+                "rawtherapee",
+                # Grafikprogramm
+                "gimp",
+                # Zeichenprogramm mit weiteren Bildbearbeitungsfunktionen
+                "krita",
+            ]
+        )

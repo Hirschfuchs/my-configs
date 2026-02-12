fbf13a59f634420a78b37067a130e9db1af4a6eb
Bono Fox <git@bono-fox.de>
2026-02-12 14:24:33 +0100

    ➕ Bestehenden Audiotreiber hinzugefügt

---
+++ modules_decman/arch_basis/audio.py
@@ -0,0 +1,23 @@
+from ..base import SubModule
+
+
+class Audio(SubModule):
+    def __init__(self):
+        super().__init__(
+            "audio",
+            native_packages=[
+                # TODO: OSS ist veraltet, es muss vollständig auf Pulse umgestellt werden
+
+                # ALSA Zentraler Audiotreiber
+                "alsa-firmware",
+                # Steuerung für ALSA
+                "alsa-utils",
+
+                # ALSA-OSS-Brücke
+                "alsa-oss"
+            ],
+            aur_packages=[
+                # Alter Open Sound System Treiber
+                "oss"
+            ]
+        )

334decb09748ace757b440443e41ece65f12feac
Bono Fox <git@bono-fox.de>
2026-02-12 11:39:08 +0100

    ➕ Weitere Kommunikationstools hinzugefügt

---
+++ modules_decman/kommunikation_weitere.py
@@ -0,0 +1,20 @@
+from base import SubModule
+
+
+class KommunikationWeitere(SubModule):
+    def __init__(self):
+        super().__init__(
+            "kommunikation-weitere",
+            native_packages=[
+                # Signal
+                "signal-desktop",
+                # Team Speak
+                "teamspeak3",
+            ],
+            aur_packages=[
+                # Webex
+                "webex-bin",
+                # Zoom
+                "zoom",
+            ]
+        )

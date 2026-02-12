175942bfcb8746b725009c5a98b24c39bb0ed137
Bono Fox <git@bono-fox.de>
2026-02-12 11:32:21 +0100

    ➕ Spotify & Tools hinzugefügt

--- hosts/techtornado_arch.py
+++ hosts/techtornado_arch.py
@@ -5,6 +5,7 @@ from modules_decman.arch_basis.firmware_treiber.gpu_nvidia import GpuNvidia
 from modules_decman.betriebssystem import Betriebssystem
 
 from modules_decman.hilfsprogramme import Hilfsprogramme
+from modules_decman.spotify import Spotify
 from modules_decman.standardprogramme import Standardprogramme
 from modules_decman.entwicklung import Entwicklung
 from modules_decman.kommunikation_privat import KommunikationPrivat
@@ -25,5 +26,6 @@ class TechtornadoArch(HostBase):
                 CpuIntel(),
                 GpuNvidia(),
                 Bildbearbeitung(),
+                Spotify(),
             ],
         )
---
+++ modules_decman/spotify.py
@@ -0,0 +1,20 @@
+from base import SubModule
+
+
+class Spotify(SubModule):
+    def __init__(self):
+        super().__init__(
+            "spotify",
+            native_packages=[
+                # Client
+                "spotify-launcher",
+            ],
+            aur_packages=[
+                # CLI-Steuerung für Spotify-Client
+                "spotify-tui",
+                # Skins für Spotify
+                "spicetify-cli",
+                # Marktplatz für Spicetify-Plugins
+                "spicetify-marketplace-bin",
+            ]
+        )

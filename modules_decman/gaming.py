9b76adebfcf7a9c5daefac7d4c7ab9082a7b7ee0
Bono Fox <git@bono-fox.de>
2026-02-12 11:56:15 +0100

    ➕ Gaming-Launcher und -Tools hinzugefügt

---
+++ modules_decman/gaming.py
@@ -0,0 +1,31 @@
+from base import SubModule
+from modules_decman.hardware.lenkrad import Lenkrad
+
+
+class Gaming(SubModule):
+    def __init__(self):
+        super().__init__(
+            "kommunikation-weitere",
+            native_packages=[
+                # Steam Client
+                "steam",
+            ],
+            aur_packages=[
+                # Cutechess Schach
+                "cutechess",
+                # Stockfish Schach Engine
+                "stockfish-git",
+                # Berserk UCI compliant Schach Engine
+                "berserk",
+                # KATEGORIE Minecraft
+                # Management & Starter für Modded & Vanilla Minecraft
+                "multimc-bin",
+                # Standard-Launcher (optional)
+                "minecraft-launcher",
+                # Technic Launcher (optional)
+                "minecraft-technic-launcher",
+            ],
+            submodules=[
+                Lenkrad(),
+            ]
+        )

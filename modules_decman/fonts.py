03bf79c4067ee06253354e937a56b36762bc55cc
Bono Fox <git@bono-fox.de>
2026-02-13 15:48:26 +0100

    ➕ Schriftarten hinzugefügt

---
+++ modules_decman/fonts.py
@@ -0,0 +1,18 @@
+from base import SubModule
+
+
+class Fonts(SubModule):
+    def __init__(self):
+        super().__init__(
+            "schriftarten",
+            native_packages=[
+                # Ubuntu Font Family
+                "ttf-ubuntu-font-family",
+                # Noto Emoji
+                "noto-fonts-emoji",
+            ],
+            aur_packages=[
+                # Dyslexia Fonts
+                "open-dyslexic-fonts",
+            ]
+        )

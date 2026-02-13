7a7ec70e499f83b7842b1e0c4486d4ecc14f56a1
Bono Fox <git@bono-fox.de>
2026-02-13 12:55:59 +0100

    ➕ Borg-Backup hinzugefügt

---
+++ modules_decman/backup.py
@@ -0,0 +1,14 @@
+from base import SubModule
+
+
+class Backup(SubModule):
+    def __init__(self):
+        super().__init__(
+            "backup",
+            native_packages=[
+                # Backend für Backups
+                "borg"
+                # Frontend für Borg Backup
+                "vorta",
+            ]
+        )

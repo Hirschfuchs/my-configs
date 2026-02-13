986b6261b36884d2f7d866209a385c0c2e0aaa4e
Bono Fox <git@bono-fox.de>
2026-02-13 12:14:59 +0100

    ➕ Apps zur Verarbeitung von Medien hinzugefügt
    - imagemagick bleibt in Hilfsprogramme, da wichtiger

---
+++ modules_decman/medien_verarbeitung.py
@@ -0,0 +1,16 @@
+from base import SubModule
+
+
+class MedienVerarbeitung(SubModule):
+    def __init__(self):
+        super().__init__(
+            "medien-verarbeitung",
+            native_packages=[
+                # Video-Transcoder
+                "handbrake",
+                # YouTube Downloader
+                "yt-dlp",
+                # Bild-zu-PDF-Konvertierer
+                "img2pdf",
+            ]
+        )

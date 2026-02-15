31d4813a07fd6ab758462ec6552d84f5e7a41efe
Bono Fox <git@bono-fox.de>
2026-02-15 11:08:54 +0100

    ➕ Streamcontroller über Flatpak hinzugefügt
    - Umzug in Flatpak, da Problem bei AUR-Installation
    - Ausführen des angefragten Gnome-Befehls

--- modules_decman/hardware/streamdeck.py
+++ modules_decman/hardware/streamdeck.py
@@ -1,12 +1,20 @@
 from ..base import SubModule
+import decman
 
 
 class Streamdeck(SubModule):
     def __init__(self):
         super().__init__(
             "streamdeck-programme",
-            aur_packages=[
+            flatpak_packages=[
                 # Streamdeck Steuerung (GUI)
-                # "streamcontroller",
+                # AUR-Installation wirft aktuell Fehler scikit_build_core._vendor.pyproject_metadata.errors.ConfigurationError
+                "com.core447.StreamController",
             ]
+        )
+
+    def after_update (self, store):
+        print("Ermögliche StreamController Gnome-Shell-Zugriff")
+        decman.prg(
+            ["flatpak", "override", "--user", "--talk-name=org.gnome.Shell.Extensions.Windows", "com.core447.StreamController"],
         )
